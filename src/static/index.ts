c6d3b4100da3b5ce8d3bbcbfb4c9655612593928
Leonardo Vargas <leu1607@hotmail.com>
2022-04-09 15:36:59 -0300

    feat: configurando a importação de svgs

---
+++ src/static/index.ts
@@ -0,0 +1,15 @@
+import IconArrowDown from './icons/arrow-down.svg'
+import IconBattleNet from './icons/battle-net.svg'
+import IconNintendo from './icons/nintendo.svg'
+import IconPlaystation from './icons/playstation.svg'
+import IconUser from './icons/user.svg'
+import IconXbox from './icons/xbox.svg'
+
+export {
+  IconArrowDown,
+  IconBattleNet,
+  IconNintendo,
+  IconPlaystation,
+  IconUser,
+  IconXbox
+}
