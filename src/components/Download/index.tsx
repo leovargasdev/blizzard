afbdaf7e2c9ae6d498a6769ce70918833787bcf2
Leonardo Vargas <leu1607@hotmail.com>
2022-04-13 21:51:06 -0300

    styles: melhorando a organização das imagens

--- src/components/Download/index.tsx
+++ src/components/Download/index.tsx
@@ -9,32 +9,34 @@ import styles from './styles.module.scss'
 
 export const Download = () => (
   <section className={styles.container}>
-    <div>
-      <div className={styles.content}>
-        <img src="/logo-battle-net.png" alt="" />
-        <h2>Baixe agora o battle.net</h2>
+    <div className={styles['image-ilustration']}>
+      <div className={styles.information}>
+        <div className={styles.content}>
+          <img src="/logo-battle-net.png" alt="" />
+          <h2>Baixe agora o battle.net</h2>
 
-        <ul>
-          <li>
-            <IconCheckGroup /> Seus jogos em um só lugar
-          </li>
-          <li>
-            <IconDots /> Conecte-se aos seus amigos
-          </li>
-          <li>
-            <IconBuy /> Compre jogos e itens digitais
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <IconCheckGroup /> Seus jogos em um só lugar
+            </li>
+            <li>
+              <IconDots /> Conecte-se aos seus amigos
+            </li>
+            <li>
+              <IconBuy /> Compre jogos e itens digitais
+            </li>
+          </ul>
 
-        <button>
-          <IconApple /> Baixar para o MacOS
-        </button>
+          <button>
+            <IconApple /> Baixar para o MacOS
+          </button>
 
-        <div className={styles['download-mobile']}>
-          <IconSmartPhone />
-          <p>
-            Também disponível como <a href="">aplicativo móvel</a>
-          </p>
+          <div className={styles['download-mobile']}>
+            <IconSmartPhone />
+            <p>
+              Também disponível como <a href="">aplicativo móvel</a>
+            </p>
+          </div>
         </div>
       </div>
     </div>
